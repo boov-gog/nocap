46305db387a8702665e20196e1b8b5624a44ef5a
gloooooory <glo8ory@gmail.com>
2024-07-24 00:00:23 -0500

    making custom button

---
+++ components/NocapButton.js
@@ -0,0 +1,52 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+
+const NocapButton = ({ onPress, title, buttonStyle, textStyle }) => {
+  const [pressed, setPressed] = useState(false);
+
+  const handlePress = () => {
+    setPressed(true);
+    onPress();
+  };
+
+  const handleBlur = () => {
+    setPressed(false);
+  };
+
+  return (
+    <Pressable onPress={handlePress} onBlur={handleBlur}>
+      {pressed ? (
+        <View style={[styles.container, buttonStyle]}>
+          <View style={styles.innerContainer}>
+            <Text style={[styles.title, textStyle]}>{title}</Text>
+          </View>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <Text style={[styles.title, textStyle]}>{title}</Text>
+        </View>
+      )}
+    </Pressable>
+  );
+};
+
+export default NocapButton;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 430,
+    height: 128,
+    paddingHorizontal: 46.5,
+    paddingVertical: 30,
+  },
+  innerContainer: {
+    borderRadius: 100,
+    backgroundColor: "white",
+  },
+  pressedContainer: {},
+  title: {
+    fontFamily: "Kanit",
+    fontSize: 32,
+    fontWeight: 700,
+  },
+});
--- config/images.js
+++ config/images.js
@@ -1,3 +1,4 @@
 export const Images = {
   logo: require("../assets/logo.png"),
+  buttonBack: require("../assets/buttonBack.png"),
 };
