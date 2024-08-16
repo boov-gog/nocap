46238f0446c5ac2819d6ea6c495d97597fe6aa49
gloooooory <glo8ory@gmail.com>
2024-08-16 12:18:25 -0500

    changed playButton in waiting room

--- components/PlayButton.js
+++ components/PlayButton.js
@@ -1,11 +1,11 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Images } from "../config";
 
 const PlayButton = ({ onPress }) => {
   const [clicked, setClicked] = useState(false);
   const [imageSrc, setImageSrc] = useState("");
-  const playEnalbed = false;
+  const playEnalbed = true;
 
   useEffect(() => {
     if (playEnalbed) {
@@ -20,39 +20,20 @@ const PlayButton = ({ onPress }) => {
   }, [clicked, playEnalbed]);
 
   const styles = StyleSheet.create({
-    container: {
-      position: "relative",
-    },
     image: {
       width: 300,
       height: 300,
     },
-    text: {
-      fontFamily: "MPR-Bold",
-      fontSize: clicked ? 32 : 40,
-      color: playEnalbed ? "#A52727" : "#BCBCBC",
-    },
-    textContainer: {
-      position: "absolute",
-      width: 300,
-      height: 286,
-      justifyContent: "center",
-      alignItems: "center",
-    },
   });
 
   return (
-    <TouchableOpacity
-      onPress={() => {}}
+    <TouchableWithoutFeedback
+      onPress={onPress}
       onPressIn={() => setClicked(true)}
       onPressOut={() => setClicked(false)}
-      style={[clicked && { opacity: 1 }, styles.container]}
     >
       <Image style={styles.image} source={imageSrc} />
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>Play!</Text>
-      </View>
-    </TouchableOpacity>
+    </TouchableWithoutFeedback>
   );
 };
 
