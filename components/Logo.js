9063a2b268f906d5967d393e1e7df9f0484f79b1
gloooooory <glo8ory@gmail.com>
2024-09-08 02:09:13 -0500

    added out style to Logo component

--- components/Logo.js
+++ components/Logo.js
@@ -4,13 +4,15 @@ import { Dimensions, Image, StyleSheet } from "react-native";
 const windowHeight = Dimensions.get("window").height;
 const windowWidth = Dimensions.get("window").width;
 
-export const Logo = ({ uri }) => {
-  return <Image source={uri} style={styles.image} resizeMode="contain" />;
+export const Logo = ({ uri, style }) => {
+  return (
+    <Image source={uri} style={[styles.image, style]} resizeMode="contain" />
+  );
 };
 
 const styles = StyleSheet.create({
   image: {
-    marginVertical: windowHeight * 0.1,
+    marginVertical: windowHeight * 0.07,
     marginHorizontal: windowWidth * 0.05,
     width: "90%",
     height: windowWidth * 0.9 * 0.31,
