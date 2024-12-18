c550d5ad69db25af9742333fab8bee3699e3968b
Diah Elisha <talenteddev18@gmail.com>
2024-12-18 22:25:47 +0700

    adjust logo width

--- components/Logo.js
+++ components/Logo.js
@@ -12,7 +12,7 @@ export const Logo = ({ uri, style }) => {
 
 const styles = StyleSheet.create({
   image: {
-    marginVertical: windowHeight * 0.015, // 0.05
+    marginVertical: windowHeight * 0.025, // 0.05
     marginHorizontal: windowWidth * 0.05,
     width: "80%", // 90%
     height: windowWidth * 0.6 * 0.30, // 0.9 * 0.31
